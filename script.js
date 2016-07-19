d6532583850c0131bbe52d59c0ebfbb4299507f5
mnaufal75 <mnaufal75@gmail.com>
2016-07-20 00:27:04 +0700

    Edited tweeting and autoreload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function changeColor() {
+    var tweet = "";
+
     var i = Math.floor(Math.random() * 7);
     var color;
+
     switch (i) {
         case 0:
             color = "#27ae60";
@@ -31,27 +34,27 @@ function changeColor() {
     document.getElementsByTagName("i")[0].style.color = color;
     document.getElementsByTagName("i")[1].style.color = color;
 
-    var j = Math.floor(Math.random() * 5);
+    var j = Math.floor(Math.random() * 4);
     switch (j) {
         case 0:
-            document.getElementsByClassName("words")[0].innerHTML = "I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.";
-            document.getElementsByClassName("author")[0].innerHTML = "Michael Jordan";
-            break;
-        case 1:
             document.getElementsByClassName("words")[0].innerHTML = "Winning isn't everything, but wanting to win is.";
             document.getElementsByClassName("author")[0].innerHTML = "Vince Lombardi";
             break;
-        case 2:
+        case 1:
             document.getElementsByClassName("words")[0].innerHTML = "Strive not to be a success, but rather to be of value.";
             document.getElementsByClassName("author")[0].innerHTML = "Albert Einstein";
             break;
-        case 3:
+        case 2:
             document.getElementsByClassName("words")[0].innerHTML = "Every strike brings me closer to the next home run.";
             document.getElementsByClassName("author")[0].innerHTML = "Babe Ruth";
             break;
-        case 4:
+        case 3:
             document.getElementsByClassName("words")[0].innerHTML = "Your time is limited, so don't waste it living someone else's life.";
             document.getElementsByClassName("author")[0].innerHTML = "Steve Jobs";
             break;
     }
+
+    tweet = 'https://twitter.com/intent/tweet?text=' +  '"' + document.getElementsByClassName("words")[0].innerHTML + '" ' + document.getElementsByClassName("author")[0].innerHTML;
+    console.log(tweet);
+    document.getElementsByTagName("a")[1].href = tweet;
 }
